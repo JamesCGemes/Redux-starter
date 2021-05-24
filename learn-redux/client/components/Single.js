ba1cc7113df643e485df7b629bae957a4f7604dc
James Gemes <james.gemes@gmail.com>
2021-05-24 16:01:03 -0500

    working on deleting posts

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,11 +1,20 @@
 import React from 'react';
-
+import Photo from './Photo';
+import Comments from './Comments';
 
 const Single = React.createClass({
     render() {
+        const {postId} = this.props.params;
+        //index of the post
+        const i =this.props.posts.findIndex((post) => post.code === postId );
+        // get us the post
+        const post = this.props.posts[i];
+        const postComments = this.props.comments[postId] || [];
+        console.log(post);
         return  (
            <div className="single-photo">
-                single photo
+        <Photo i={i} post={post} {...this.props}/>
+               <Comments postComments={postComments} {...this.props}/>
            </div>
         )
     }
--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -4,9 +4,17 @@
 // copy of the current state.
 
 function posts(state = [], action) {
-    console.log(state, action);
-    return state;
-
+    switch (action.type){
+        case 'INCREMENT_LIKES' :
+            const i = action.index;
+            return [
+                ...state.slice(0,i),//everything before the one we are updating
+                {...state[i], likes: state [i].likes + 1},
+        ...state.slice(i + 1),//everything after the one we are updating
+            ]
+        default:
+            return state;
+    }
 }
 
 export default posts;
